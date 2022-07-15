b94174244375fdd25a5869cd47beb26c5cd3bbd6
dnp176 <dnp176.developer@gmail.com>
2022-07-15 19:45:45 +0530

    remove testimonial and add technologies code

--- src/components/technologies/technologies.jsx
+++ src/components/technologies/technologies.jsx
@@ -1,70 +1,127 @@
 import React from 'react'
-import './testimonial.css'
-import Av1 from '../../assets/avatar1.jpg'
-import Av2 from '../../assets/avatar2.jpg'
-import Av3 from '../../assets/avatar3.jpg'
-import Av4 from '../../assets/avatar4.jpg'
-
-// import Swiper core and required modules
-import { Pagination, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import './technologies.css'
+import IMG1 from '../../assets/programming/php.png'
+import IMG2 from '../../assets/programming/laravel.png'
+import IMG3 from '../../assets/programming/python.png'
+import IMG4 from '../../assets/programming/django.png'
+import IMG5 from '../../assets/programming/bootstrap.png'
+import IMG6 from '../../assets/programming/js.png'
+import IMG7 from '../../assets/programming/html-5.png'
+import IMG8 from '../../assets/programming/css.png'
+import IMG9 from '../../assets/programming/nodejs.png'
+import IMG10 from '../../assets/programming/postgre.png'
+import IMG11 from '../../assets/programming/mysql.png'
+import IMG12 from '../../assets/programming/github.png'
+import IMG13 from '../../assets/programming/atom.png'
 
 const data = [
   {
-    avatar:Av1,
-    name:'Client Name1',
-    review:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+    id:1,
+    image:IMG1,
   },
   {
-    avatar:Av2,
-    name:'Client Name2',
-    review:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+    id:2,
+    image:IMG2,
   },
   {
-    avatar:Av3,
-    name:'Client Name3',
-    review:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+    id:3,
+    image:IMG3,
   },
   {
-    avatar:Av4,
-    name:'Client Name4',
-    review:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+    id:4,
+    image:IMG4,
+  },
+  {
+    id:5,
+    image:IMG5,
+  },
+  {
+    id:6,
+    image:IMG6,
+  },
+  {
+    id:7,
+    image:IMG7,
+  },
+  {
+    id:8,
+    image:IMG8,
+  },
+  {
+    id:9,
+    image:IMG9,
+  },
+  {
+    id:10,
+    image:IMG10,
+  },
+  {
+    id:11,
+    image:IMG11,
+  },
+  {
+    id:12,
+    image:IMG12,
+  },
+  {
+    id:13,
+    image:IMG13,
   }
 ]
 
-export const Testimonials = () => {
+export const Technologies = () => {
   return (
-    <section id='testimonials'>
+    <section id='technologies'>
       <h5>Review from clients</h5>
-      <h2>Testimonials</h2>
+      <h2>Technologies</h2>
 
-      <Swiper className="container testimonials__container"
-        // install Swiper modules
-        modules={[ Pagination, A11y]}
-        spaceBetween={40}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-      >
-       {
-          data.map(({avatar,name,review},index) => {
-            return (
-              <SwiperSlide key={index} className='testimonial'>
-                <div className="client__avatar">
-                  <img src={avatar} alt='img'/>
-                </div>
-                <h5 className='client__name'>{name}</h5>
-                <small className='client__review'>{review}</small>
-              </SwiperSlide>
-            )
-          })
-       }
-      </Swiper>
+      <div className="container technology__container">
+
+        {
+            data.map(({id,image,title,github,demo}) => {
+              return(
+                <artical key={id} className='technology_item'>
+                  <div className="technology__item-image">
+                    <img src={image} alt=''/>
+                  </div>
+                </artical>
+              )
+            })
+          }
+
+        {/* <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical>
+
+        <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical>
+        <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical>
+        <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical>
+        <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical>
+        <artical className='technology_item'>
+          <div className="technology__item-image">
+              <img src={IMG1} alt='' width="50"/>
+          </div>
+        </artical> */}
+      </div>
     </section>
   )
 }
-export default Testimonials
+export default Technologies
