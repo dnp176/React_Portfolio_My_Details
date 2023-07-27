cc167302751936d9bcc3007297cda9a59ab44fdd
dnp176 <dnp176.developer@gmail.com>
2023-07-27 23:44:59 +0530

    tooltrip added and analytics testing code add

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,28 @@ import Portfolio from './components/portfolio/Portfolio'
 import Technologies from './components/technologies/Technologies'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import ReactGA from 'react-ga';
+import {useEffect } from 'react';
+// import ReactDOM from 'react-dom';
+// import ReactGA from 'react-ga';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+const TRACKING_ID = "G-F3P097DYC6"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+// import ReactDOM from 'react-dom';
+
+const useAnalyticsEventTracker = (category="Blog category") => {
+  const eventTracker = (action = "test action", label = "test label") => {
+    ReactGA.event({category, action, label});
+  }
+  return eventTracker;
+}
 
 export const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <Header />
--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -21,91 +21,112 @@ import IMG18 from '../../assets/programming/ai.png'
 import IMG19 from '../../assets/programming/ml.png'
 import IMG20 from '../../assets/programming/web_analytics.png'
 import IMG21 from '../../assets/programming/gitlab.png'
-
+// var tooltripval = ""
 const data = [
   {
     id:3,
     image:IMG3,
+    tooltripval:"Python",
   },
   {
     id:18,
     image:IMG18,
+    tooltripval:"Artificial Intelligence",
   },
   {
     id:19,
     image:IMG19,
+    tooltripval:"Machine Learning",
   },
   {
     id:20,
     image:IMG20,
+    tooltripval:"Data Analysis",
   },
   {
     id:14,
     image:IMG14,
+    tooltripval:"Java",
   },
   {
     id:15,
     image:IMG15,
+    tooltripval:"Data Scientist",
   },
   {
     id:16,
     image:IMG16,
+    tooltripval:"Open CV",
   },
   {
     id:17,
     image:IMG17,
+    tooltripval:"Selenium",
   },
   {
     id:10,
     image:IMG10,
+    tooltripval:"Postgre SQL",
   },
   {
     id:11,
     image:IMG11,
+    tooltripval:"MySQL",
   },
   {
     id:12,
     image:IMG12,
+    tooltripval:"Github",
   },
   {
     id:21,
     image:IMG21,
+    tooltripval:"Gitlab",
   },
   {
     id:4,
     image:IMG4,
+    tooltripval:"Django",
   },
   {
     id:5,
     image:IMG5,
+    tooltripval:"Bootstrap",
   },
   {
     id:6,
     image:IMG6,
+    tooltripval:"Javascript",
   },
   {
     id:7,
     image:IMG7,
+    tooltripval:"HTML",
   },
   {
     id:8,
     image:IMG8,
+    tooltripval:"CSS",
   },
   {
     id:9,
     image:IMG9,
+    tooltripval:"Node JS",
   },
   {
     id:13,
     image:IMG13,
+    tooltripval:"React JS",
   },
   {
     id:1,
     image:IMG1,
+    tooltripval:"PHP",
   },
   {
     id:2,
     image:IMG2,
+    tooltripval:"Laravel",
   },
 ]
 
@@ -118,11 +139,12 @@ export const Technologies = () => {
       <div className="container technology__container">
 
         {
-            data.map(({id,image,title,github,demo}) => {
+            data.map(({id,image,tooltripval}) => {
               return(
                 <artical key={id} className='technology_item'>
                   <div className="technology__item-image">
-                    <img src={image} alt=''/> 
+                    <img src={image} alt=''/> <span class="tooltiptext">{tooltripval}</span>
+                    {/* <ReactTooltip className="custom-tooltip" effect="solid" /> */}
                   </div>
                 </artical>
               )
